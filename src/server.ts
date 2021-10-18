cd93c5e0bd0092fe6e63894368c264c2f7ecc7a2
williamwjd <william.jose.wjd@gmail.com>
2021-10-18 19:42:43 -0300

    configurando oauth do github e rota de autenticaçao

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
 
-app.listen(3333,()=>{
+app.get('/github', (req, res)=>{
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get('/signin/callback', (req, res)=>{
+    const { code } = req.query;
+
+    return res.json(code);
+})
+
+app.listen(4000,()=>{
     console.log('Servidor online')
 })
